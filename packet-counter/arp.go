5804fc6a04de27b1957e195451c1e07d6f9123a5
Hugo Jeller <hugo_jeller@hotmail.com>
2021-04-09 15:54:51 -0400

    Implemented packet counter based on arpscan example from gopacket

---
+++ packet-counter/arp.go
@@ -0,0 +1,132 @@
+// Copyright 2012 Google, Inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
+
+// arpscan implements ARP scanning of all interfaces' local networks using
+// gopacket and its subpackages.  This example shows, among other things:
+//   * Generating and sending packet data
+//   * Reading in packet data and interpreting it
+//   * Use of the 'pcap' subpackage for reading/writing
+
+// https://github.com/google/gopacket/blob/master/examples/arpscan/arpscan.go
+package main
+
+import (
+	"errors"
+	"flag"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"log"
+	"net"
+	"sync"
+)
+
+var (
+	ifacename = flag.String("iface", "Ethernet", "Interface name")
+	deviceid = flag.String("deviceid", "eth0", "Interface device ID")
+)
+
+func main() {
+	flag.Parse()
+
+	log.Printf("Interface device ID %v", *deviceid)
+
+	// Get a list of all interfaces.
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, iface := range ifaces {
+		wg.Add(1)
+		// Start up a scan on each interface.
+		go func(iface net.Interface) {
+			defer wg.Done()
+			if iface.Name != *ifacename {
+				return
+			}
+			if err := scan(&iface, *deviceid); err != nil {
+				log.Printf("interface %v: %v", iface.Name, err)
+			}
+		}(iface)
+	}
+	// Wait for all interfaces' scans to complete.  They'll try to run
+	// forever, but will stop on an error, so if we get past this Wait
+	// it means all attempts to write have failed.
+	wg.Wait()
+}
+
+// scan scans an individual interface's local network for machines using ARP requests/replies.
+//
+// scan loops forever, sending packets out regularly.  It returns an error if
+// it's ever unable to write a packet.
+func scan(iface *net.Interface, deviceid string) error {
+	// We just look for IPv4 addresses, so try to find if the interface has one.
+	var addr *net.IPNet
+	addresses, err := iface.Addrs()
+	if err != nil {
+		return err
+	}
+
+	for _, a := range addresses {
+		ipnet, ok := a.(*net.IPNet)
+		if !ok {
+			continue
+		}
+
+		ip4 := ipnet.IP.To4()
+		if ip4 == nil {
+			continue
+		}
+
+		addr = &net.IPNet{
+			IP:   ip4,
+			Mask: ipnet.Mask[len(ipnet.Mask)-4:],
+		}
+		break
+	}
+
+	// Sanity-check that the interface has a good address.
+	if addr == nil {
+		return errors.New("no good IP network found")
+	}
+	log.Printf("Using network range %v for interface %v", addr, iface.Name)
+
+	// Open up a pcap handle for packet reads/writes.
+	handle, err := pcap.OpenLive(deviceid, 65536, true, pcap.BlockForever)
+	if err != nil {
+		return err
+	}
+	defer handle.Close()
+
+	read(handle)
+
+	return nil
+}
+
+// read watches a handle for incoming ARP responses we might care about, and prints them.
+//
+// read loops until 'stop' is closed.
+func read(handle *pcap.Handle) {
+	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+	in := src.Packets()
+	counter := make(map[string]int)
+	for {
+		packet := <-in
+		ipLayer := packet.Layer(layers.LayerTypeIPv4)
+		if ipLayer == nil {
+			continue
+		}
+		ipv4 := ipLayer.(*layers.IPv4)
+
+		counter[ipv4.SrcIP.String()]++
+		// Note:  we might get some packets here that aren't responses to ones we've sent,
+		// if for example someone else sends US an ARP request.  Doesn't much matter, though...
+		// all information is good information :)
+		log.Printf("Received %v packets from %v", counter[ipv4.SrcIP.String()], ipv4.SrcIP)
+	}
+}
---
+++ packet-counter/go.mod
@@ -0,0 +1,5 @@
+module example.com/packet-counter
+
+go 1.16
+
+require github.com/google/gopacket v1.1.19
