38822f6855b30163d99499aa3263bd29c2d762ef
Hugo Jeller <hugo_jeller@hotmail.com>
2021-04-12 01:46:30 -0400

    Added rate-limiting. Fixed byte header. Added response signature check.

--- valve-master-server/master.go
+++ valve-master-server/master.go
@@ -1,65 +1,100 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
 	"sync"
+	"time"
 )
 
 var (
-	header = []byte{0x31, 0xff}
-	zeros  = []byte{0x00, 0x00} // TODO actually build filters (first zero is from IP)
-	seeds  = make(chan string)
-	ips    = make(chan string)
+	header   = []byte{0x31}
+	response = []byte{0xff, 0xff, 0xff, 0xff, 0x66, 0x0a}
+	seeds    = make(chan string)
+	ips      = make(chan string)
+)
+
+const (
+	ZeroIp string = "0.0.0.0:0"
+	// regions
+	UsEastCoast  byte = 0x00
+	UsWestCoast  byte = 0x01
+	SouthAmerica byte = 0x02
+	Europe       byte = 0x03
+	Asia         byte = 0x04
+	Australia    byte = 0x05
+	MiddleEast   byte = 0x06
+	Africa       byte = 0x07
+	RestOfWorld  byte = 0xff
 )
 
 //printIps receives any parsed IP and adds to main list
 func printIps() {
 	var all []string
 	for {
-		ip := <- ips
+		ip := <-ips
 		all = append(all, ip)
 		fmt.Printf("Received %d: %s\n", len(all), ip)
 	}
 }
 
+func ToZeroString(s string) []byte {
+	return append([]byte(s), 0x00)
+}
+
 //sendQuery send master server queries
 func sendQuery(conn *net.UDPConn) {
+	ratelimit := time.Tick(time.Second)
 	for {
 		seed := <-seeds
-		fmt.Println("Seed: " + seed)
 
-		parts := [][]byte{header, []byte(seed), zeros}
-
-		var result []byte
-		for _, p := range parts {
-			result = append(result, p...)
+		packetParts := [][]byte{
+			header,
+			{SouthAmerica},
+			ToZeroString(seed),
+			ToZeroString("\\dedicated\\1\\linux\\1\\empty\\1\\password\\0\\appid\\730"),
 		}
 
-		// TODO rate limit
-		_, err := conn.Write(result)
+		var packet []byte
+		for _, part := range packetParts {
+			packet = append(packet, part...)
+		}
+
+		_, err := conn.Write(packet)
 		if err != nil {
 			fmt.Printf("Couldn't send response %v\n", err)
 		}
+
+		<- ratelimit
 	}
 }
 
 //receiveData receives data from UDP connection
 func receiveData(conn *net.UDPConn) {
-	p := make([]byte, 2048)
+	buffer := make([]byte, 2048)
 
 	for {
-		_, _, err := conn.ReadFromUDP(p)
+		_, _, err := conn.ReadFromUDP(buffer)
+
 		if err != nil {
 			fmt.Printf("Some error  %v", err)
 			continue
 		}
-		var last string
-		for i := 0; ; i += 6 {
-			ip := sliceToIp(p[i : i+6])
 
-			if ip == "0.0.0.0:0" {
+		// Check if response has correct header
+		if bytes.Compare(response, buffer[0:6]) != 0 {
+			fmt.Printf("Invalid response: %v", err)
+			continue
+		}
+
+		var last string
+		for i := 6; ; i += 6 {
+			ip := sliceToIp(buffer[i : i+6])
+
+			if ip == ZeroIp {
 				break
 			}
 			ips <- ip
@@ -82,9 +117,15 @@ func sliceToIp(data []byte) string {
 
 //main kickstart everything
 func main() {
+	ip, err := net.ResolveIPAddr("ip4", "hl2master.steampowered.com")
+
+	if err != nil {
+		log.Panic(err)
+	}
+
 	addr := net.UDPAddr{
 		Port: 27011,
-		IP:   net.ParseIP("208.64.200.52"), // FIXME: resolve from hostname
+		IP:   ip.IP, // FIXME: resolve from hostname
 	}
 
 	fmt.Println("Dialing connection")
@@ -100,7 +141,7 @@ func main() {
 	go receiveData(conn)
 	go printIps()
 
-	seeds <- "0.0.0.0:0"
+	seeds <- ZeroIp
 
 	// Wait forever
 	// TODO: this should become a timeout
